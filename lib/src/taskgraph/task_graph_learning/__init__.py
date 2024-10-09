89c918b733830c29ce818ce8b5294efbe4207a2d
Luigi Seminara <seminara.luigi@gmail.com>
2024-10-09 19:07:08 +0200

    fix(lib): TGT model

--- lib/src/taskgraph/task_graph_learning/__init__.py
+++ lib/src/taskgraph/task_graph_learning/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) FPV@IPLab, and its affiliates. All Rights Reserved.
 
 from ._loss import task_graph_maximum_likelihood_loss
-from ._models import DO
+from ._models import DO, TGT
 from .baselines._baseline import baseline_ILP, baseline_transition_graph, save_graph_as_svg
 from ._utils import *
--- lib/src/taskgraph/task_graph_learning/_models.py
+++ lib/src/taskgraph/task_graph_learning/_models.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import numpy as np
 
 from ._loss import task_graph_maximum_likelihood_loss
+import torch.nn.functional as F
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
 
 
 class TaskGraph(nn.Module):
@@ -44,3 +46,65 @@ class DO(nn.Module):
         values = self.get_adjacency_matrix()
         # We sum eps to avoid log(0)
         return task_graph_maximum_likelihood_loss(y, values + eps, beta)
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, num_heads, dropout):
+        super(Encoder, self).__init__()
+        encoder_layers = TransformerEncoderLayer(input_dim, num_heads, hidden_dim, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, num_layers)
+    
+    def forward(self, src):
+        output = self.transformer_encoder(src)
+        return output
+
+class TGT(nn.Module):
+    def __init__(self, d_model=4096, n_heads=1, n_layers=1, d_ff=4096, dropout=0.1, device='cpu'):
+        super(TGT, self).__init__()
+        self.embedding_start = nn.Embedding(2, d_model)
+        self.embedding_end = nn.Embedding(2, d_model)
+        self.transformer = Encoder(d_model, d_ff, n_layers, n_heads, dropout)
+        self.f = nn.Sequential(
+            nn.TransformerEncoderLayer(d_model*2, nhead=2, dropout=dropout, dim_feedforward=2048),
+            nn.Linear(d_model*2, 16),
+            nn.TransformerEncoderLayer(16, nhead=2, dropout=dropout, dim_feedforward=2048),
+            nn.Linear(16, 1)
+        )
+        self.softmax = nn.Softmax(dim=1)
+        self.device = device
+    
+    def get_adjacency_matrix(self, vectors, custom_mask=None):
+        vectors = torch.cat((self.embedding_start.weight, vectors, self.embedding_end.weight), dim=0)
+        vectors = self.transformer(vectors.unsqueeze(0)).squeeze()
+        vectors = vectors.to(self.device)
+        ii,jj=torch.meshgrid(torch.arange(len(vectors)),torch.arange(len(vectors)), indexing='ij')
+        ii = ii.ravel()
+        jj = jj.ravel()
+        rel_features=torch.cat([vectors[ii],vectors[jj]],-1)
+        out = self.f(rel_features)
+        values = out.view(len(vectors),len(vectors))
+        mask = np.ones((values.size()[0], values.size()[0]), dtype=bool)
+        mask[0, :] = False
+        mask[1, :] = False
+        mask[1, 0] = True
+        mask[:, -1] = False
+        mask[:, -2] = False
+        mask[-1, :] = False
+        mask[-1, -2] = True
+        if custom_mask is not None:
+            action_mask = (mask) & (~np.identity(values.size()[0], dtype=bool)) & (custom_mask)
+        else:
+            action_mask = (mask) & (~np.identity(values.size()[0], dtype=bool))
+        action_mask = torch.tensor(action_mask, dtype=torch.bool).to(self.device)
+        large_negative = torch.tensor(-1e10).to(self.device)
+        softmax_input = torch.where(action_mask, values, large_negative)
+        values = self.softmax(softmax_input)
+        return values, vectors
+
+    def forward(self, vectors, y, eps, beta, mask=None):
+        values, vectors = self.get_adjacency_matrix(vectors, mask)
+        vectors = F.normalize(vectors, p=2, dim=-1)
+        vectors = vectors @ vectors.T
+        vectors *= torch.exp(torch.tensor(0.9).to(self.device))
+        labels = torch.arange(len(vectors), device = self.device) 
+        loss = F.cross_entropy(vectors, labels) * (len(vectors))
+        return task_graph_maximum_likelihood_loss(y, values + eps, beta), loss
